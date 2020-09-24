dff30136db58244b49597be0c69c14b2d33db766
Albert Kolozsvari <albertartk@gmail.com>
2020-09-24 17:07:29 +0100

    Create version 1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="canonicalwebteam.image-template",
-    version="1.2.0",
+    version="1.3.0",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url=(
